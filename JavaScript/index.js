04b7c908544b8af1d5a1e856624d6b33f4b1261a
Vishal Jaiswada <vp022570@gmail.com>
2024-02-21 03:05:18 +0530

    modified index js

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,2 +1,38 @@
 // line below is used to print on the console window
 console.log('This is JS Version 3.0');
+let a=5;
+console.log(a);
+let lastName ="Jai Shree Ram";
+console.log(lastName);
+let name1='Jai Seeta Ram';
+console.log(name1);
+var name2='Sankat Mochan hanuman';
+console.log(name2);
+const num=5.5;
+console.log(num);
+
+a="Vishal Jaiswada";
+console.log(a);
+
+let person={
+    firstName:'Vishal',
+    age:22
+};
+
+console.log(person);
+console.log(person.age);
+console.log(person['firstName']);
+
+let num1=2;
+let num2=5;
+let c=num1+num2;
+console.log(c);
+c=num1**num2;
+console.log(c);
+c=num1%num2;
+console.log(c);
+
+let age=22;
+
+let status1=(age>=18)?'Yes Vote':'No Vote';
+console.log(status1);
