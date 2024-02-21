24bc14b3c9f2c8508e90b1be9bd715ae69289899
Vishal Jaiswada <vp022570@gmail.com>
2024-02-21 19:38:24 +0530

    Modified index js file

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -36,3 +36,34 @@ let age=22;
 
 let status1=(age>=18)?'Yes Vote':'No Vote';
 console.log(status1);
+
+let marks=55;
+if(marks>=90){
+    console.log('A');
+}
+else if(marks>=80){
+    console.log('B');
+}
+else if(marks>=70){
+    console.log('C');
+}
+else if(marks>=60){
+    console.log('D');
+}
+else if(marks>=50){
+    console.log('E');
+}
+else{
+    console.log('F');
+}
+
+let number=2;
+switch(number){
+    case 1: console.log('A');
+    break;
+    case 2: console.log('B');
+    break;
+    case 3: console.log('C');
+    break;
+    default: console.log('D');
+}
