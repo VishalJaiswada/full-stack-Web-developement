5cd5db15b7cdae6c237bc026f2d1ec6185bc79c1
Vishal Jaiswada <vp022570@gmail.com>
2024-02-22 02:48:36 +0530

    Added index1.html and index1.js file

---
+++ JavaScript/index1.js
@@ -0,0 +1,13 @@
+// object creation in js
+
+let rectangle ={
+    length:7,
+    breadth:9,
+
+    area: function(){
+        console.log('Area of rectangle:'+rectangle.length*rectangle.breadth);
+    }
+};
+console.log(rectangle);
+ 
+
